f432bf2de920bdd0e0b44eca3bf461abdddc29df
Annalise Smith <smit7am@mail.uc.edu>
2026-02-04 12:54:31 -0500

    straight to bucharest

--- romania_map.py
+++ romania_map.py
@@ -20,3 +20,25 @@ map = {
     'Iasi': [('Vaslui', 92), ('Neamt', 87)],
     'Neamt': [('Iasi', 87)]
 }
+
+straight_to_bucharest = {
+    'Arad': 366, 
+    'Bucharest': 0, 
+    'Craiova': 160, 
+    'Drobeta': 242,
+    'Eforie': 161, 
+    'Fagaras': 178, 
+    'Giurgiu': 77, 
+    'Hirsova': 151,
+    'Iasi': 226, 
+    'Lugoj': 244, 
+    'Mehadia': 241, 
+    'Neamt': 234,
+    'Oradea': 380, 
+    'Pitesti': 98, 
+    'Rimnicu Vilcea': 193,
+    'Sibiu': 253, 
+    'Timisoara': 329, 
+    'Urziceni': 80, 
+    'Vaslui': 199,
+    'Zerind': 374}
